32941a70b4c848221457d32de27163a752c88d44
jnavarrete85 <xmljson94y@gmail.com>
2020-02-07 21:04:13 -0800

    add middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const auth = require('../../middleware/auth');
 
 /**
  * @route GET api/auth
  * @desc Test route
- * @access Public
+ * @access Private
  */
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res)=>{
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch (e) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
