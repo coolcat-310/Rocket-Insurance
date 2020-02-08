e277a6b91a30d1fedf5fd9c21f82f2379f2db285
jnavarrete85 <xmljson94y@gmail.com>
2020-02-07 23:11:11 -0800

    add client for react

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -126,4 +126,25 @@ router.get('/user/:user_id', async (req, res)=>{
     }
 });
 
+/**
+ * @route    DELETE api/profile
+ * @desc     Delete profile, user & posts
+ * @access   Private
+ */
+
+router.delete('/', auth,async (req, res)=>{
+    try {
+        // Remove User's post
+        // await Post.deleteMany({ user: req.user.id });
+        // Remove profile
+        await Profile.findOneAndRemove({user: req.user.id});
+        // Remove user
+        await User.findOneAndRemove({_id: req.user.id});
+        res.json({msg: 'User Deleted'});
+    }catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
